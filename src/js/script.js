df7fd6a7b005bafd8a17c6953a676ed45e7cd3fc
Efim <efim0424@gmail.com>
2023-06-09 11:32:25 +0300

    We apply conditions and cycles

--- src/js/script.js
+++ src/js/script.js
@@ -6,11 +6,26 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = +prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = +prompt("На сколько оцените его?", "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+  const latestMovies = prompt("Один из последних просмотренных фильмов?", ""),
+    ratingMovies = +prompt("На сколько оцените его?", "");
+  if (latestMovies !== null && ratingMovies !== null && latestMovies !== "" && ratingMovies !== "" && latestMovies.length < 50) {
+    personalMovieDB.movies[latestMovies] = ratingMovies;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
